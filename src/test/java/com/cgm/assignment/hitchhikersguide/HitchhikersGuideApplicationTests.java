79242fe152243ac469fef506372120cbf289f097
Ankit Agrawal <aag080@CRB.APMOLLER.NET>
2020-06-30 15:17:57 +0200

    skeleton

---
+++ src/test/java/com/cgm/assignment/hitchhikersguide/HitchhikersGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgm.assignment.hitchhikersguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitchhikersGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
